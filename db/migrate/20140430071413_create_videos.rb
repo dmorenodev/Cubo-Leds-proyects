724cbe6761a9293d3982375b16f32d6041341325
dmoreno19949 <dmorenodev@gmail.com>
2014-05-08 15:13:45 +0200

    videos subidos desde el you tube y funcionando

--- db/migrate/20140430071413_create_videos.rb
+++ db/migrate/20140430071413_create_videos.rb
@@ -2,7 +2,7 @@ class CreateVideos < ActiveRecord::Migration
   def change
     create_table :videos do |t|
       t.string :titulo
-      t.text :description
+      t.string :description
 
       t.timestamps
     end
