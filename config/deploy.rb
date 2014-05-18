bc7245b8a4b04e401405811bc110de113f0c9629
dmoreno19949 <dmorenodev@gmail.com>
2014-05-18 12:58:29 +0200

    Gemfile modificado

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
-set :application, 'Cubo-Leds-proyects'
-set :repo_url, 'https://github.com/dmorenodev/Cubo-Leds-proyects'
+set :application, 'my_app_name'
+set :repo_url, 'git@example.com:me/my_repo.git'
 
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
